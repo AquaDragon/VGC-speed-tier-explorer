d8f60af6efdb1df6d2b55d8ecee93dde3fd5c646
Shawn <22651173+AquaDragon@users.noreply.github.com>
2023-09-20 12:34:29 -0700

    Add speed boosts from Abilites & Booster Energy

--- README.md
+++ README.md
@@ -2,14 +2,14 @@
 
 A tool for exploring Pokémon speed tiers in various VGC formats. Optimized for Pokémon Scarlet & Violet.
 
-**Note:** Currently in development and some features may not work.
+**Note:** Currently in development and some features may not work correctly.
 
 https://aquadragon.github.io/vgc-speed-tier-explorer/
 
 ### To-do (not in any order):
-- Add boosts from abilities: Protosynthesis/Quark Drive (x1.5), weather-based abilities (x2), Unburden (x2), Commander (+2)
-- Add boosts from moves: Tailwind (x2), Dragon Dance/Quiver Dance (+1)
-- Group regional formes into a single entry if they have the same base speed
+- Add Tailwind boosts (x2)
+- Add stage boosts: Dragon Dance/Quiver Dance (+1), Commander (+2)
+- Group regional formes into a single entry if they have the same speed
 - Update visuals: sprites, buttons & layout
 - Allow user to add custom entries to the table
 
--- js/tablescripts.js
+++ js/tablescripts.js
@@ -11,6 +11,7 @@ var cboxFastBST = document.getElementById('cboxFastBST');
 var cboxSlowBST = document.getElementById('cboxSlowBST');
 var cboxChoiceScarf = document.getElementById('cboxChoiceScarf');
 var cboxIronBall = document.getElementById('cboxIronBall');
+var cboxAbilities = document.getElementById('cboxAbilities');
 
 // Initialize initial sort orders & display filters
 var isAscending = false; // descending by default
@@ -19,6 +20,9 @@ var setFastBST = parseInt(selectFastBST.value, 10);
 var setSlowBST = parseInt(selectSlowBST.value, 10);
 var setChoiceScarfBST = parseInt(selectChoiceScarfBST.value, 10);
 var setIronBallBST = parseInt(selectIronBallBST.value, 10);
+
+const abilityList = ['Chlorophyll', 'Swift Swim', 'Sand Rush', 'Slush Rush', 'Unburden']; // x2
+const abParadox = ['Quark Drive', 'Protosynthesis']; // x1.5 with Booster Energy
 
 // Function to reset the table to its initial state
 function defaultTableRules() {
@@ -44,41 +48,43 @@ function defaultTableRules() {
   selectIronBallBST.value = '50';
   setIronBallBST = parseInt(selectIronBallBST.value, 10);
 
+  cboxAbilities.checked = true;
+
   clearTable();
 }
 
 function calcSpeedStat(base, ivs, evs, nature) {
   const level = 50;
   const natureBoost = nature === '+' ? 1.1 : nature === '-' ? 0.9 : 1;
-  return Math.floor(
-    (Math.floor(((base * 2 + ivs + Math.floor(evs / 4)) * level) / 100) + 5) * natureBoost
-  );
+  return Math.floor((Math.floor(((base * 2 + ivs + Math.floor(evs / 4)) * level) / 100) + 5) * natureBoost);
 }
 
 // Update the button texts based on current display settings
 function updateButtonText() {
-  btnFilterNonFullyEvolved.textContent = hasNonFullyEvolved
-    ? 'Exclude Non-Fully Evolved'
-    : 'Include Non-Fully Evolved';
+  btnFilterNonFullyEvolved.textContent = hasNonFullyEvolved ? 'Exclude Non-Fully Evolved' : 'Include Non-Fully Evolved';
   btnSortAscending.textContent = isAscending ? 'Sort Descending' : 'Sort Ascending';
 }
 
-function generateTableEntry(pokemonName, baseStat, ivs, evs, nature, item) {
+function generateTableEntry(pokeName, baseStat, ivs, evs, nature, item, ability) {
   let speedStat = 0;
-  if (item === 'Choice Scarf') {
+
+  if (['Choice Scarf', 'Booster Energy'].includes(item)) {
     speedStat = Math.floor(calcSpeedStat(baseStat, ivs, evs, nature) * 1.5);
   } else if (item === 'Iron Ball') {
     speedStat = Math.floor(calcSpeedStat(baseStat, ivs, evs, nature) * 0.5);
+  } else if (abilityList.includes(ability)) {
+    speedStat = calcSpeedStat(baseStat, ivs, evs, nature) * 2;
   } else {
     speedStat = calcSpeedStat(baseStat, ivs, evs, nature);
   }
   return {
-    name: pokemonName,
+    name: pokeName,
     baseStat: baseStat,
     iv: ivs,
     ev: evs,
     nature: nature,
     item: item,
+    ability: ability,
     stat: speedStat,
   };
 }
@@ -90,39 +96,59 @@ function generateTableData() {
   const format = selectedFormat ? sortAlphaGetKeys(selectedFormat) : sortAlphaGetKeys(POKEDEX_SV);
 
   // Loop through the sorted names and generate data for each Pokémon
-  format.forEach(function (pokemonName) {
-    const pokemon = POKEDEX_SV_NATDEX[pokemonName];
+  format.forEach(function (pokeName) {
+    const pokemon = POKEDEX_SV_NATDEX[pokeName];
 
     if (hasNonFullyEvolved || !pokemon.canEvolve) {
       const baseStat = pokemon.bs.sp;
       let ivs, evs, nature, stat, pokemonData;
+      const ability = pokemon.ab; // this is default ab display on the damage calc
+      const ability2 = pokemon.ab2 ? pokemon.ab2 : null; // check for 2nd ability
 
       // Neutral speed (always populate)
-      pokemonData = generateTableEntry(pokemonName, baseStat, 31, 0, '', null);
+      pokemonData = generateTableEntry(pokeName, baseStat, 31, 0, '', null, null);
       tableData.push(pokemonData);
 
       // Fast speed
       if (baseStat >= setFastBST && cboxFastBST.checked) {
-        pokemonData = generateTableEntry(pokemonName, baseStat, 31, 252, '+', null);
+        pokemonData = generateTableEntry(pokeName, baseStat, 31, 252, '+', null, null);
         tableData.push(pokemonData);
       }
 
       // Slow speed
       if (baseStat <= setSlowBST && cboxSlowBST.checked) {
-        pokemonData = generateTableEntry(pokemonName, baseStat, 0, 0, '-', null);
+        pokemonData = generateTableEntry(pokeName, baseStat, 0, 0, '-', null, null);
         tableData.push(pokemonData);
       }
 
-      // Choice Scarf
+      // Item: Choice Scarf / Booster Energy (paradox mons)
       if (baseStat >= setChoiceScarfBST && cboxChoiceScarf.checked) {
-        pokemonData = generateTableEntry(pokemonName, baseStat, 31, 252, '+', 'Choice Scarf');
+        if (abParadox.includes(ability)) {
+          pokemonData = generateTableEntry(pokeName, baseStat, 31, 252, '+', 'Booster Energy', null);
+        } else {
+          pokemonData = generateTableEntry(pokeName, baseStat, 31, 252, '+', 'Choice Scarf', null);
+        }
         tableData.push(pokemonData);
       }
 
-      // Iron Ball
+      // Item: Iron Ball
       if (baseStat <= setIronBallBST && cboxIronBall.checked) {
-        pokemonData = generateTableEntry(pokemonName, baseStat, 0, 0, '-', 'Iron Ball');
+        pokemonData = generateTableEntry(pokeName, baseStat, 0, 0, '-', 'Iron Ball', null);
         tableData.push(pokemonData);
+      }
+
+      // Abilities: Weather boost
+      if (cboxAbilities.checked && abilityList.includes(ability)) {
+        pokemonData = generateTableEntry(pokeName, baseStat, 31, 252, '+', null, ability); // Max speed
+        tableData.push(pokemonData);
+        pokemonData2 = generateTableEntry(pokeName, baseStat, 31, 252, '', null, ability); // 252 speed EVs, neutral nature
+        tableData.push(pokemonData2);
+      }
+      if (cboxAbilities.checked && abilityList.includes(ability2)) {
+        pokemonData = generateTableEntry(pokeName, baseStat, 31, 252, '+', null, ability2); // Max speed
+        tableData.push(pokemonData);
+        pokemonData2 = generateTableEntry(pokeName, baseStat, 31, 252, '', null, ability2); // 252 speed EVs, neutral nature
+        tableData.push(pokemonData2);
       }
     }
   });
@@ -143,7 +169,7 @@ function generateTable() {
     <div class="table-cell">BST</div>
     <div class="table-cell">IVs</div>
     <div class="table-cell">EVs</div>
-    <div class="table-cell">Item</div>
+    <div class="table-cell mods">Item / Ability</div>
     <div class="table-cell poke-names">Pokémon</div>
   `;
   displayTable.appendChild(tableHeader);
@@ -159,7 +185,7 @@ function generateTable() {
   // Group Pokémon with identical baseStat, iv, and evs
   const groupedPokemon = {};
   pokemonData.forEach(function (poke) {
-    const key = `${poke.baseStat}-${poke.iv}-${poke.ev}-${poke.item}`;
+    const key = `${poke.baseStat}-${poke.iv}-${poke.ev}-${poke.nature}-${poke.item}-${poke.ability}`;
     if (!groupedPokemon[key]) {
       groupedPokemon[key] = [];
     }
@@ -171,7 +197,8 @@ function generateTable() {
     if (groupedPokemon.hasOwnProperty(key)) {
       const pokemonGroup = groupedPokemon[key];
       const stat = pokemonGroup[0].stat;
-      const pokemonNames = pokemonGroup.map((poke) => poke.name).join(', ');
+      const pokeNames = pokemonGroup.map((poke) => poke.name).join(', ');
+      const mods = [pokemonGroup[0].item, pokemonGroup[0].ability].filter(Boolean).join(', ');
 
       var tableRow = document.createElement('div');
       tableRow.classList.add('table-row');
@@ -180,14 +207,8 @@ function generateTable() {
         <div class="table-cell">${pokemonGroup[0].baseStat}</div>
         <div class="table-cell">${pokemonGroup[0].iv}</div>
         <div class="table-cell">${pokemonGroup[0].ev}${pokemonGroup[0].nature}</div>
-        <div class="table-cell">
-          ${
-            pokemonGroup[0].item !== null && pokemonGroup[0].item !== ''
-              ? pokemonGroup[0].item
-              : '-'
-          }
-        </div>
-        <div class="table-cell poke-names">${pokemonNames}</div>`;
+        <div class="table-cell mods">${mods ? mods : '-'}</div>
+        <div class="table-cell poke-names">${pokeNames}</div>`;
       displayTable.appendChild(tableRow);
     }
   }
@@ -241,6 +262,7 @@ cboxEventListener(cboxFastBST);
 cboxEventListener(cboxSlowBST);
 cboxEventListener(cboxChoiceScarf);
 cboxEventListener(cboxIronBall);
+cboxEventListener(cboxAbilities);
 
 // Event listeners for select value changes
 function addChangeListener(s, updateFunction) {
