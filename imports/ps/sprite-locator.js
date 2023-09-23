66f083460ad6eda9e84fe77c3bcdb0a3aa66ed1b
Shawn <22651173+AquaDragon@users.noreply.github.com>
2023-09-23 13:18:19 -0700

    Prettier to include sprite-locator.js

--- imports/ps/sprite-locator.js
+++ imports/ps/sprite-locator.js
@@ -7,39 +7,37 @@
 */
 
 function toID(text) {
-    var _text, _text2;
+  var _text, _text2;
 
-    if ((_text = text) != null && _text.id) {
-        text = _text.id;
-    } else if ((_text2 = text) != null && _text2.userid) {
-        text = _text2.userid;
-    }
+  if ((_text = text) != null && _text.id) {
+    text = _text.id;
+  } else if ((_text2 = text) != null && _text2.userid) {
+    text = _text2.userid;
+  }
 
-    if (typeof text !== 'string' && typeof text !== 'number') {
-        return '';
-    }
+  if (typeof text !== 'string' && typeof text !== 'number') {
+    return '';
+  }
 
-    return ('' + text).toLowerCase().replace(/[^a-z0-9]+/g, '');
+  return ('' + text).toLowerCase().replace(/[^a-z0-9]+/g, '');
 }
 
 function getItemIcon(item) {
-    var _item;
-    var num = 0;
+  var _item;
+  var num = 0;
 
-    if (typeof item === 'string' && PS_BATTLE_ITEMS) {
-        item = PS_BATTLE_ITEMS[toID(item)];
-    } 
+  if (typeof item === 'string' && PS_BATTLE_ITEMS) {
+    item = PS_BATTLE_ITEMS[toID(item)];
+  }
 
-    if ((_item = item) != null && _item.spritenum) {
-        num = _item.spritenum;
-    }
+  if ((_item = item) != null && _item.spritenum) {
+    num = _item.spritenum;
+  }
 
-    var top = Math.floor(num / 16) * 24;
-    var left = num % 16 * 24;
+  var top = Math.floor(num / 16) * 24;
+  var left = (num % 16) * 24;
 
-    var spritesheetURL = 'assets/itemicons-sheet.png'
+  var spritesheetURL = 'assets/itemicons-sheet.png';
 
-    return 'background: transparent url(' + spritesheetURL + ') no-repeat scroll -' +
-        left + 'px -' + top + 'px';
-};
-
+  return 'background: transparent url(' + spritesheetURL + ') no-repeat scroll -' + left + 'px -' + top + 'px';
+}
